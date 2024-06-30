a230681199706a26858f7aa01d39b7d1f92360be
Alex Lepin <alexlepin@mail.ru>
2024-06-30 17:38:11 +0300

    fix: redesign the showRepairServices func

---
+++ scripts/showRepairServices.js
@@ -0,0 +1,35 @@
+"use strict";
+const showBtns = document.querySelectorAll(".show-more");
+
+if (showBtns) {
+  showBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => showMoreBtnHandler(e));
+  });
+}
+
+function showMoreBtnHandler(e) {
+  // нашел все необходимые эелементы
+  const showBtn = e.target;
+  const serviceSection = e.target.closest(".service");
+  const brandsList = serviceSection.querySelector(".service__swiper");
+  const showImg = serviceSection.querySelector(".more__img");
+  const liElement = brandsList.querySelector(".service__item");
+  let liHeight = parseInt(getComputedStyle(liElement).height);
+  // начальная высота блока brandsList
+  const initBrandsListHeight = "160px";
+  // высота блока brandsList после клика по кнопке showBtn
+  let greaterBrandsListHeight = `${
+    parseInt(initBrandsListHeight) + liHeight + 16
+  }px`;
+  // отслеживаем через изменение класса
+  brandsList.classList.toggle("service__swiper--height");
+  if (brandsList.classList.contains("service__swiper--height")) {
+    brandsList.style.height = greaterBrandsListHeight;
+    showBtn.textContent = "Скрыть";
+    showImg.style.transform = "rotate(180deg)";
+  } else {
+    brandsList.style.height = initBrandsListHeight;
+    showBtn.textContent = "Показать все";
+    showImg.style.transform = "rotate(0deg)";
+  }
+}
