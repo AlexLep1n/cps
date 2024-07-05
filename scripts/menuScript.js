550fc6574bbaae5d6a68fdfa2fa816edd7a921ec
Alex Lepin <alexlepin@mail.ru>
2024-07-05 15:00:58 +0300

    feat: implement close menu by click on overlay

--- scripts/menuScript.js
+++ scripts/menuScript.js
@@ -5,12 +5,20 @@ const menuOverlay = document.querySelector(".overlay--z--50");
 const menuBlock = document.querySelector(".menu");
 const menuCloseBtn = document.querySelector(".nav-button__menu-close");
 
+// Open by menu open button
 menuOpenBtn.addEventListener("click", function () {
   menuBlock.classList.toggle("menu--show");
   menuOverlay.classList.toggle("overlay--hidden");
 });
 
+// Close by menu close button
 menuCloseBtn.addEventListener("click", function () {
+  menuBlock.classList.toggle("menu--show");
+  menuOverlay.classList.toggle("overlay--hidden");
+});
+
+// Close by menu overlay
+menuOverlay.addEventListener("click", function () {
   menuBlock.classList.toggle("menu--show");
   menuOverlay.classList.toggle("overlay--hidden");
 });
