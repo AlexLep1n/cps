3f26600cc707f40cba3e673d441447f69454fab7
Alex Lepin <alexlepin@mail.ru>
2024-07-06 13:20:13 +0300

    feat: implement func for menu active link and menu active lang button

--- scripts/menuScript.js
+++ scripts/menuScript.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Script for open and close menu
 const menuOpenBtn = document.querySelector(".nav-button__menu");
 const menuOverlay = document.querySelector(".overlay--z--50");
 const menuBlock = document.querySelector(".menu");
@@ -21,4 +22,32 @@ menuCloseBtn.addEventListener("click", function () {
 menuOverlay.addEventListener("click", function () {
   menuBlock.classList.toggle("menu--show");
   menuOverlay.classList.toggle("overlay--hidden");
+});
+
+// Script for change active menu link
+const menuLinks = document.querySelectorAll(".menu__nav-link");
+let menuActiveLink = document.querySelector(".menu__nav-link--active");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("menu__nav-link--active")) {
+      menuActiveLink.classList.remove("menu__nav-link--active");
+      e.target.classList.add("menu__nav-link--active");
+      menuActiveLink = e.target;
+    }
+  });
+});
+
+// Script for change active language button
+const menuLangBtn = document.querySelectorAll(".menu__footer-btn");
+let menuActiveLangBtn = document.querySelector(".menu__footer-btn--active");
+
+menuLangBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("menu__footer-btn--active")) {
+      menuActiveLangBtn.classList.remove("menu__footer-btn--active");
+      e.target.classList.add("menu__footer-btn--active");
+      menuActiveLangBtn = e.target;
+    }
+  });
 });
